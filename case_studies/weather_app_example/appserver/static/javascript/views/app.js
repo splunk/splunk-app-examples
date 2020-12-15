9d3e8a6dec5f9ab61b3e1096bcdbb1d9a2e783a9
Tim Pavlik <tpavlik@splunk.com>
2020-12-14 17:48:46 -0800

    Add oneshot search to http://localhost:8000/en-US/app/weather_app_example/setup_page_dashboard

--- case_studies/weather_app_example/appserver/static/javascript/views/app.js
+++ case_studies/weather_app_example/appserver/static/javascript/views/app.js
@@ -12,6 +12,35 @@ import * as Setup from "./setup_page.js";
 define(["react", "splunkjs/splunk"], function(react, splunk_js_sdk){
   const e = react.createElement;
 
+  var app_name = "weather_app_example";
+
+  var application_name_space = {
+    owner: "nobody",
+    app: app_name,
+    sharing: "app",
+  };
+
+  var http = new splunk_js_sdk.SplunkWebHttp();
+  var options = {id: "id_" + ((new Date()).valueOf())};
+
+  var splunk_js_sdk_service = new splunk_js_sdk.Service(
+    http,
+    application_name_space,
+  );
+  console.log("splunk_js_sdk_service: ");
+  console.log(splunk_js_sdk_service);
+
+  var jobs = splunk_js_sdk_service.jobs({app: 'weather_app_example'});
+  var req = jobs.oneshotSearch('search index=_internal | head 1', options, function(err, job) {
+    if (err) {
+      console.warn("oneshotSeach failed");
+      console.warn(err);
+      return;
+    }
+    console.log("oneshotSeach succeeded");
+    console.log(job);
+  });
+
   class SetupPage extends react.Component {
     constructor(props) {
       super(props);
