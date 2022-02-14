ea2a435836547c7b78b9ade7cb757548e85e251b
Abhi Shah <abhis@splunk.com>
2022-02-14 13:55:50 +0530

    python examples
    command line python sdk examples

---
+++ python/inputs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Copyright 2011-2015 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""A command line utility for interacting with Splunk inputs."""
+
+import sys
+
+from splunklib.client import connect
+
+from utils import parse
+
+def main():
+    opts = parse(sys.argv[1:], {}, ".env")
+    service = connect(**opts.kwargs)
+
+    for item in service.inputs:
+        header =  "%s (%s)" % (item.name, item.kind)
+        print(header)
+        print('='*len(header))
+        content = item.content
+        for key in sorted(content.keys()):
+            value = content[key]
+            print("%s: %s" % (key, value))
+        print()
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ python/search_modes.py
@@ -0,0 +1,37 @@
+import sys
+from utils import *
+import time
+from splunklib.client import connect
+from splunklib import results
+
+def cmdline(argv, flags, **kwargs):
+    """A cmdopts wrapper that takes a list of flags and builds the
+       corresponding cmdopts rules to match those flags."""
+    rules = dict([(flag, {'flags': ["--%s" % flag]}) for flag in flags])
+    return parse(argv, rules, ".env", **kwargs)
+
+def modes(argv):
+    opts = cmdline(argv, [])
+    kwargs_splunk = dslice(opts.kwargs, FLAGS_SPLUNK)
+    service = connect(**kwargs_splunk)
+
+    # By default the job will run in 'smart' mode which will omit events for transforming commands
+    job = service.jobs.create('search index=_internal | head 10 | top host')
+    while not job.is_ready():
+        time.sleep(0.5)
+        pass    
+    reader = results.ResultsReader(job.events())
+    # Events found: 0
+    print('Events found with adhoc_search_level="smart": %s' % len([e for e in reader]))
+
+    # Now set the adhoc_search_level to 'verbose' to see the events
+    job = service.jobs.create('search index=_internal | head 10 | top host', adhoc_search_level='verbose')
+    while not job.is_ready():
+        time.sleep(0.5)
+        pass
+    reader = results.ResultsReader(job.events())
+    # Events found: 10
+    print('Events found with adhoc_search_level="verbose": %s' % len([e for e in reader]))
+
+if __name__ == "__main__":
+    modes(sys.argv[1:])
