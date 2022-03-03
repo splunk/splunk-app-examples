cfc928abdd7b35682f79483a9c74cb71050816d1
vmalaviya <vmalaviya@splunk.com>
2022-03-03 12:11:03 +0530

    Update utils.py

--- python/utils.py
+++ python/utils.py
@@ -12,7 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-"""Utility module shared by the SDK examples & unit tests."""
+"""Utility module shared by the SDK examples """
 
 import sys
 from os import path
