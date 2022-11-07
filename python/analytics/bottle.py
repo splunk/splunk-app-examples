a17ad6c5dbc68f61f702ebe8b4dd7b885aa19819
akaila-splunk <akaila@splunk.com>
2022-11-07 15:39:14 +0530

    Update bottle.py

--- python/analytics/bottle.py
+++ python/analytics/bottle.py
@@ -1252,9 +1252,9 @@ class _ImportRedirect:
         ''' Create a virtual package that redirects imports (see PEP 302). '''
         self.name = name
         self.impmask = impmask
-        # print(name)
+        
         self.module = sys.modules.setdefault(name, imp.new_module(name))
-        # self.module = sys.modules.setdefault(name, types.ModuleType(name))
+
         self.module.__dict__.update({'__file__': __file__, '__path__': [],
                                      '__all__': [], '__loader__': self})
         sys.meta_path.append(self)
